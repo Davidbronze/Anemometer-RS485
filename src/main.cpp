9a128be0ad90b8ce5656088b53635296dedf6880
Davidbronze <davidmolles1965@hotmail.com>
2024-02-20 17:53:05 -0300

    different lib, trying to fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <Arduino.h>
 // github link: https://github.com/4-20ma/ModbusMaster
 #include <ModbusMaster.h> // só serve para dados de 32 bits
+#include <ModbusRTUMaster.h>
 #include <SPI.h>
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
@@ -17,12 +18,13 @@
 #define MODBUS_TX_PIN 19 // Tx pin 19 of ESP32 connect to DI pin of MAX485
 #define MODBUS_SERIAL_BAUD 4800 // Baud rate for esp32 and max485 communication
 
-// voltage, current and frequency data register of DDM18SD
-uint16_t data_register[2] = {0x0000, 0x0001};
 
-//Initialize the ModbusMaster object as node1
-// ModbusMaster node1; // a lib ModbusMaster é somente para slaves que armazenam dados em 32 bits
-// ModbusMaster node2;
+ModbusRTUMaster modbus(Serial2, MODBUS_DIR_PIN); // serial port, driver enable pin for rs-485 (optional)
+
+uint16_t inputSpeed[2]; // array to storage the holding registers of anemometer
+uint16_t inputDirection[2]; // array to storage holding registers of wind direction
+
+
 
 // Pin 4 made high for Modbus transmision mode
 void modbusPreTransmission()
@@ -39,125 +41,51 @@ void modbusPostTransmission()
 
 
 void setup()
-{
-  //  esp serial communication
-  Serial.begin(115200);
-  pinMode(MODBUS_DIR_PIN, OUTPUT);
-  digitalWrite(MODBUS_DIR_PIN, LOW);
+      {
+        //  esp serial communication
+        Serial.begin(115200);
+        pinMode(MODBUS_DIR_PIN, OUTPUT);
+        digitalWrite(MODBUS_DIR_PIN, LOW);
 
-  //Serial2.begin(baud-rate, protocol, RX pin, TX pin);.
-  Serial2.begin(MODBUS_SERIAL_BAUD, SERIAL_8E1, MODBUS_RX_PIN, MODBUS_TX_PIN);
-  Serial2.setTimeout(2000);    
-  
+        //Serial2.begin(baud-rate, protocol, RX pin, TX pin);.
+        Serial2.begin(MODBUS_SERIAL_BAUD, SERIAL_8E1, MODBUS_RX_PIN, MODBUS_TX_PIN);
+        Serial2.setTimeout(2000);
+        modbus.begin(4800);   
 
-  //modbus slave ID 1
-  // node1.begin(1, Serial2);
-  // node2.begin(2, Serial2);
-
-//  callbacks allow us to configure the RS485 transceiver correctly
-//   node1.preTransmission(modbusPreTransmission);
-//   node1.postTransmission(modbusPostTransmission);
-//   node2.preTransmission(modbusPreTransmission);
-//   node2.postTransmission(modbusPostTransmission);
-
-   Serial.println("end of setup");
-  
-  }
+        Serial.println("end of setup");
+        
+        }
 
   void sendModBus (){
-    modbusPreTransmission();    
-   uint8_t modBusCommand[8] = {0x01, 0x03, 0x00, 0x00, 0x00, 0x02, 0xC4, 0x0B};
-   for (int i=0; i<=8; i++){
-    Serial2.print(modBusCommand[i]);
-   }
-   modbusPostTransmission();
-   Serial.println("command sent");
-  }
+          modbusPreTransmission();    
+        uint8_t modBusCommand[8] = {0x01, 0x03, 0x00, 0x00, 0x00, 0x02, 0xC4, 0x0B};
+        for (int i=0; i<=8; i++){
+          Serial2.print(modBusCommand[i]);
+        }
+        modbusPostTransmission();
+        Serial.println("command sent");
+        }
 
   void receiveModBus(){
-    if (Serial2.available()){
-      char dataBit = Serial2.read();      
-      uint8_t modBusResponse[] = {};
-    }
-  }
+        if (Serial2.available()){
+          char dataBit = Serial2.read();      
+          uint8_t modBusResponse[] = {};
+        }
+      }
 
 void loop()
 {
-    uint8_t result;
-    uint16_t data[2];
-    int i;    
-    float reading;
+    // slave id, starting data address,
+    // unsigned 16 bit integer array to place input register values, number of input registers to read:
+    modbus.readInputRegisters(1, 0, inputSpeed, 2); 
+    modbus.readInputRegisters(2, 0, inputDirection, 2);
 
-    uint8_t result2;
-    uint16_t data2[2];
-    int j;
-    float reading2;
+    Serial.print("Wind speed: ");
+    Serial.println(inputSpeed[0]);
+    Serial.println(*((float *)inputSpeed[0]));
+    Serial.print("Wind direction: ");
+    Serial.println(inputDirection[1]);
+    Serial.println(*((float *)inputDirection[1]));                                                        
 
 
-
-
-
-      
-      /* //Modbus function 0x03 Read Holding Registers according to energy meter datasheet
-      result = node1.readHoldingRegisters(0x0000, 1);
-      Serial.println("first reading");
-      Serial.println(node1.getResponseBuffer(0x01));
-        if (result == node1.ku8MBSuccess) {
-          Serial.println("Success, Received data: ");
-          
-          //Retrieve the data from getResponseBuffer(uint8_t u8Index) function.
-          //that is return 16-bit data. our energy meter return 32-bit data everytime.
-          //that's why, we take the value to a array called data
-           data[0]=node1.getResponseBuffer(0x00);
-           data[1]=node1.getResponseBuffer(0x01);
-           
-           //read voltage          
-            Serial.print("Wind speed: ");
-            // we just convert the uint16_t type data array to float type using type casting
-            reading = *((float *)data);
-            Serial.print(reading/10); // conversion to m/s
-            Serial.println(" m/s  ou ");
-            Serial.print(reading*0.36); //conversion to km/h
-            Serial.println(" km/h");
-           
-           
-        } else {
-          Serial.print("Slave 1 failed, Response Code: ");
-          Serial.print(result, HEX);
-          Serial.println("");
-          delay(5000); 
-        }
-
-    result2 = node2.readHoldingRegisters(0, 1);
-    Serial.println(node2.getResponseBuffer(0x00));
-    Serial.println(node2.getResponseBuffer(0x01));
-    Serial.println(node2.getResponseBuffer(0x02));
-    Serial.println(node2.getResponseBuffer(0x03));
-    Serial.println(node2.getResponseBuffer(0x04));
-        if (result2 == node2.ku8MBSuccess) {
-          Serial.println("Success, Received data: ");
-          
-          //Retrieve the data from getResponseBuffer(uint8_t u8Index) function.
-          //that is return 16-bit data. our energy meter return 32-bit data everytime.
-          //that's why, we take the value to a array called data
-           data2[0]=node2.getResponseBuffer(0x00);
-           data2[1]=node2.getResponseBuffer(0x01);
-           
-           //read voltage          
-            Serial.print("Wind direction: ");
-            // we just convert the uint16_t type data array to float type using type casting
-            reading2 = *((float *)data2);
-            Serial.print(" Azimuth = ");
-            Serial.print(reading2);
-            Serial.println(" grades");
-           
-           
-        } else {
-          Serial.print("Slave 2 failed, Response Code: ");
-          Serial.print(result, HEX);
-          Serial.println("");
-          delay(5000); 
-        } 
-    
-    delay(400); */
   }
